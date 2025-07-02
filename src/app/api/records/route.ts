c7592c21bd92736e9ef3858eb50c5101ba2cc6eb
Rodion <rodson5973@gmail.com>
2025-07-02 14:31:50 +0300

    production fix2

--- src/app/api/records/route.ts
+++ src/app/api/records/route.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import { auth } from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
 
 export async function GET(req: NextRequest) {
     console.log("record route HIT");
-const authResult = await auth(req);
-const userId = "userId" in authResult ? authResult.userId : null;
+const {userId} =  getAuth(req);
+
 
 if (!userId) {
   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
